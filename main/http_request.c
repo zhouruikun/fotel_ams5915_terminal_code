27bdee2841d436356d8eb0bc8b6321ed85455495
zhouruikun <915684080@qq.com>
2019-02-10 19:06:40 +0800

    fix http request

--- main/http_request.c
+++ main/http_request.c
@@ -9,20 +9,22 @@
 #include "http_request.h"
 
 /* Constants that aren't configurable in menuconfig */
-// #define WEB_URL "192.168.0.106"
+// #define WEB_URL "/dev/put_data"
 // #define WEB_SERVER "192.168.0.106"
-#define WEB_PORT "6666"
-#define WEB_URL "106.14.226.150"
+#define WEB_PORT "7513"
+#define WEB_URL "/dev/put_data"
 #define WEB_SERVER "106.14.226.150"
 static const char *TAG = "http";
 
 static const char *REQUEST = "GET " WEB_URL " HTTP/1.0\r\n"
-    "Host: "WEB_SERVER"\r\n"
+    "Host: "WEB_SERVER":"WEB_PORT"\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
+    "content-length: %d"
     "\r\n\r\n" ;
-
+char * Header[100] = {0};
 char str_asm[80];
-
+ 
+ 
 void ICACHE_FLASH_ATTR  http_get_task(void *pvParameters)
 {
     const struct addrinfo hints = {
@@ -41,7 +43,7 @@ void ICACHE_FLASH_ATTR  http_get_task(void *pvParameters)
         */
         xEventGroupWaitBits(wifi_event_group, CONNECTING_BIT,
                             false, false, portMAX_DELAY);
-        ESP_LOGI(TAG, "Connected to AP");
+        ESP_LOGI(TAG, "Connecting to AP");
 
         int err = getaddrinfo(WEB_SERVER, WEB_PORT , &hints, &res);
 
@@ -76,22 +78,31 @@ void ICACHE_FLASH_ATTR  http_get_task(void *pvParameters)
 
         ESP_LOGI(TAG, "... connected");
         freeaddrinfo(res);
-
         str_request = generate_str();
-        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
+
+        char *str_request_all = (char *) malloc(strlen(REQUEST) + strlen(str_request));
+        // sprintf(str_request_all, "%s%s", REQUEST, str_request);
+        strcpy(str_request_all, REQUEST);
+        sprintf(Header,REQUEST,strlen(str_request));
+        strcpy(str_request_all, Header);
+        strcat(str_request_all, str_request);
+        if (write(s, str_request_all, strlen(str_request_all)) < 0) {
             ESP_LOGE(TAG, "... socket send failed");
             close(s);
             vTaskDelay(4000 / portTICK_PERIOD_MS);
+            cJSON_free(str_request);
+            free(str_request_all);
             continue;
         }
-        // ESP_LOGE(TAG,"%s\n",str_request );
-        if (write(s, str_request, strlen(str_request)) < 0) {
-            ESP_LOGE(TAG, "... socket send failed");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            free(str_request);
-            continue;
-        }
+        // // ESP_LOGE(TAG,"%s\n",str_request );
+        // if (write(s, str_request, strlen(str_request)) < 0) {
+        //     ESP_LOGE(TAG, "... socket send failed");
+        //     close(s);
+        //     vTaskDelay(4000 / portTICK_PERIOD_MS);
+        //     cJSON_free(str_request);
+        //     continue;
+        // }
+         free(str_request_all);
         cJSON_free(str_request);
         ESP_LOGI(TAG, "... socket send success");
 
